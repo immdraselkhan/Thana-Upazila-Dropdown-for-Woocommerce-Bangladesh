7507a4a87cff2063996c2f41f91f0893380c518d
Md Rasel Khan <52624987+immdraselkhan@users.noreply.github.com>
2022-05-10 10:09:36 +0600

    Add files via upload

---
+++ city-dropdown-for-woocommerce-bd/states/BD.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+ * States of Bangladesh
+ * Author: Md Rasel Khan <hello@raselnotes.com> | https://raselnotes.com
+ * Version: 1.0.0
+ * License: http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+global $states;
+
+$states['BD'] = array(
+	"BD-05" => "Bagerhat",
+	"BD-01" => "Bandarban",
+	"BD-02" => "Barguna",
+	"BD-06" => "Barishal",
+	"BD-07" => "Bhola",
+	"BD-03" => "Bogura",
+	"BD-04" => "Brahmanbaria",
+	"BD-09" => "Chandpur",
+	"BD-10" => "Chattogram",
+	"BD-12" => "Chuadanga",
+	"BD-11" => "Cox's Bazar",
+	"BD-08" => "Cumilla",
+	"BD-13" => "Dhaka",
+	"BD-14" => "Dinajpur",
+	"BD-15" => "Faridpur ",
+	"BD-16" => "Feni",
+	"BD-19" => "Gaibandha",
+	"BD-18" => "Gazipur",
+	"BD-17" => "Gopalganj",
+	"BD-20" => "Habiganj",
+	"BD-21" => "Jamalpur",
+	"BD-22" => "Jashore",
+	"BD-25" => "Jhalokati",
+	"BD-23" => "Jhenaidah",
+	"BD-24" => "Joypurhat",
+	"BD-29" => "Khagrachhari",
+	"BD-27" => "Khulna",
+	"BD-26" => "Kishoreganj",
+	"BD-28" => "Kurigram",
+	"BD-30" => "Kushtia",
+	"BD-31" => "Lakshmipur",
+	"BD-32" => "Lalmonirhat",
+	"BD-36" => "Madaripur",
+	"BD-37" => "Magura",
+	"BD-33" => "Manikganj ",
+	"BD-39" => "Meherpur",
+	"BD-38" => "Moulvibazar",
+	"BD-35" => "Munshiganj",
+	"BD-34" => "Mymensingh",
+	"BD-48" => "Naogaon",
+	"BD-43" => "Narail",
+	"BD-40" => "Narayanganj",
+	"BD-42" => "Narsingdi",
+	"BD-44" => "Natore",
+	"BD-45" => "Nawabganj",
+	"BD-41" => "Netrakona",
+	"BD-46" => "Nilphamari",
+	"BD-47" => "Noakhali",
+	"BD-49" => "Pabna",
+	"BD-52" => "Panchagarh",
+	"BD-51" => "Patuakhali",
+	"BD-50" => "Pirojpur",
+	"BD-53" => "Rajbari",
+	"BD-54" => "Rajshahi",
+	"BD-56" => "Rangamati",
+	"BD-55" => "Rangpur",
+	"BD-58" => "Satkhira",
+	"BD-62" => "Shariatpur",
+	"BD-57" => "Sherpur",
+	"BD-59" => "Sirajganj",
+	"BD-61" => "Sunamganj",
+	"BD-60" => "Sylhet",
+	"BD-63" => "Tangail",
+	"BD-64" => "Thakurgaon",
+	);
+return $states;
